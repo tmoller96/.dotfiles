18d5879f704a18fe42b0ac9e52b513441ee061b5
tmoller96 <48474561+tmoller96@users.noreply.github.com>
2025-05-17 21:09:41 +0200

    Update install oh-my-zsh step

--- mac/setup-mac.sh
+++ mac/setup-mac.sh
@@ -32,7 +32,7 @@ brew bundle --file="$BREWFILE"
 # 2. Install Oh My Zsh if not present
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "[+] Installing Oh My Zsh..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 else
   echo "[+] Oh My Zsh already installed."
 fi
